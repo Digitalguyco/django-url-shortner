f5d3f23c1caba25e49f8d66c6b6312c66fd5847a
josh <globalcompanyworks@gmail.com>
2022-07-20 04:17:20 -0700

    make it better

--- urlshortner/urls.py
+++ urlshortner/urls.py
@@ -19,7 +19,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('secret/admin/', admin.site.urls),
     # App(urlshort) Urls Include
     path('', include('urlshort.urls'))
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # Adds Static Url
